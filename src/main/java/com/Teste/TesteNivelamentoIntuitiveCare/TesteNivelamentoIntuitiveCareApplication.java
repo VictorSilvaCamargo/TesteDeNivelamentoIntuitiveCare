570c58685928dd98b9dcb6b7471f51fbe93f9235
Victor S. Camargo <victorscamargo51@gmail.com>
2024-03-24 09:54:44 -0300

    first commit

---
+++ src/main/java/com/Teste/TesteNivelamentoIntuitiveCare/TesteNivelamentoIntuitiveCareApplication.java
@@ -0,0 +1,13 @@
+package com.Teste.TesteNivelamentoIntuitiveCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteNivelamentoIntuitiveCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteNivelamentoIntuitiveCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Teste/TesteNivelamentoIntuitiveCare/TesteNivelamentoIntuitiveCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teste.TesteNivelamentoIntuitiveCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteNivelamentoIntuitiveCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
